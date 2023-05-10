63fe4b6aef1ffe3a297e5130f01cd2b6c60759ed
Densup <ggden22@gmail.com>
2023-05-10 17:39:56 +0300

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,20 @@
   import { initialFen } from '../utility/fen';
 
   let clicked = false;
+  let checked = false;
   let url = '';
+  let dialogHidden = true;
   $: fullUrl = $page.url.href + url;
+  $: checked, (clicked = false);
 
   function copy() {
     navigator.clipboard.writeText(fullUrl);
+    if (dialogHidden) {
+      dialogHidden = false;
+      setTimeout(() => {
+        dialogHidden = true;
+      }, 1200);
+    }
   }
 
   async function handle() {
@@ -19,53 +28,197 @@
     url = await response.text();
     clicked = true;
 
-    const newGame = { fen: initialFen, side: 'white', gameUrl: url };
-    if (!socket.connected) socket.connect();
+    const newGame = {
+      fen: initialFen,
+      side: !checked ? 'white' : 'black',
+      gameUrl: url
+    };
+
+    if (!socket.connected) {
+      socket.connect();
+
+      socket.on('connect_error', (err) => {
+        console.log('connection error!');
+        console.log(err);
+      });
+      socket.once('player connect', () => {
+        goto('/' + url);
+      });
+    }
 
     socket.emit('init game', newGame);
-
-    socket.on('connect_error', (err) => {
-      console.log('connection error!');
-      console.log(err);
-    });
-    socket.once('player connect', () => {
-      goto('/' + url);
-    });
   }
 </script>
 
 <div class="window">
-  <h1>CHECKERS</h1>
-  <label for="link">send this link to your friend!</label>
-  <input type="text" id="link" readonly bind:value={url} on:click={copy} />
-  <button class:invisible={clicked} on:click|once={handle}>Play!</button>
-  <button class:invisible={!clicked} disabled
-    >Waiting for other player to join...</button
-  >
+  <h1>Начать игру</h1>
+  <div class="side">
+    <span class:hide={checked}>Белые</span>
+    <input type="checkbox" id="switch" class="checkbox" bind:checked />
+    <label for="switch" class="toggle" />
+    <span class:hide={!checked}>Черные</span>
+  </div>
+  {#if !clicked}
+    <button class="btn" on:click={handle}>Начать</button>
+  {:else}
+    <div class="link">
+      <label for="link">Отправь эту ссылку другу:</label>
+      <div class="link-button">
+        <button id="link" on:click={copy}>{fullUrl}</button>
+        <div class="dialog" class:hidden={dialogHidden}>
+          <p>копировано</p>
+        </div>
+      </div>
+    </div>
+  {/if}
 </div>
 
 <style>
   .window {
+    font-size: 1.5rem;
+    height: 15em;
+    width: 15em;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-    gap: 1rem;
-    border-radius: 1rem;
-    background-color: aliceblue;
     padding: 1rem;
+    gap: 2rem;
+    border-radius: 10px;
+    background-color: #bbeafe;
     user-select: none;
   }
-  .window button {
-    padding: 8px;
+  .window > *:last-child {
+    margin-top: auto;
+    margin-bottom: 2rem;
   }
 
-  #link {
-    /* pointer-events: none; */
+  .side {
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+  }
+  .side .hide {
+    opacity: 0;
+  }
+  .side > span {
+    transition: opacity ease-in 0.25s;
+  }
+  .side > span:first-child {
+    margin-right: 1rem;
+  }
+  .side > span:last-child {
+    margin-left: 1rem;
+  }
+
+  .toggle {
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: 2em;
+    height: 1em;
+    background-color: black;
+    border-radius: 1em;
     cursor: pointer;
+    transition: background-color ease-in-out 0.25s;
+  }
+  .toggle:after {
+    content: '';
+    position: absolute;
+    width: 0.9em;
+    height: 0.9em;
+    border-radius: 50%;
+    background-color: white;
+    left: 0.1em;
+    transition: background-color ease-in-out 0.25s, transform ease-in-out 0.25s;
+  }
+  .checkbox:checked + .toggle::after {
+    transform: translateX(100%);
+    background-color: black;
+  }
+  .checkbox:checked + .toggle {
+    background-color: white;
+  }
+  .checkbox:focus-visible + .toggle {
+    outline: 2px solid blue;
+  }
+  .checkbox {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  .btn {
+    font-size: 1em;
+    padding: 8px;
+    width: 65%;
+    border: 2px solid white;
+    border-radius: 6px;
+    font-weight: bold;
+    color: black;
+    background-color: white;
+    cursor: pointer;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+    transition: background-color ease-in 50ms;
+  }
+  .btn:hover {
+    background-color: hsl(0, 0%, 98%);
+  }
+  .btn:active {
+    background-color: #bbeafe;
+    box-shadow: none;
   }
 
-  .invisible {
-    display: none;
+  .link {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  }
+  .link > label {
+    font-size: smaller;
+  }
+  .link-button {
+    position: relative;
+    display: flex;
+    align-items: center;
+  }
+  .dialog {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    left: calc(100% + 0.5rem);
+    font-size: 1rem;
+    border: none;
+    border-radius: 8px;
+    padding: 4px;
+    background-color: hsl(0, 0%, 20%);
+    color: white;
+    z-index: 10;
+    transition: opacity ease-in 0.2s;
+  }
+  .dialog.hidden {
+    opacity: 0;
+  }
+  .dialog::before {
+    content: '';
+    position: absolute;
+    background-color: red;
+    background-color: hsl(0, 0%, 20%);
+    z-index: -1;
+    width: 10px;
+    height: 10px;
+    left: -3px;
+    transform: rotate(45deg);
+  }
+  #link {
+    width: 100%;
+    background-color: white;
+    /* border: 4px solid black; */
+    cursor: pointer;
+    border: none;
+    padding: 8px;
+    border-radius: 1rem;
+    transition: transform ease-in 50ms;
+  }
+  #link:active {
+    transform: scale(0.97);
   }
 </style>
